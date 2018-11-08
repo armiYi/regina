d3bf35c21ad8bdadc7e90eaca8c963b00a972d34
EC2 Default User <ec2-user@ip-172-31-11-36.us-east-2.compute.internal>
2018-11-08 15:40:06 +0000

    Day 1 : Mac sample routes controller html

---
+++ regina/app/controllers/eric_controller.rb
@@ -0,0 +1,16 @@
+class EricController < ApplicationController
+
+    def home
+    end
+    
+    def help
+    end
+    
+    def index
+    end
+    
+    def show
+    end
+
+    
+end
--- regina/config/routes.rb
+++ regina/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    # resources :eric
+    root "eric#home"
+    get "eric/home"
+    get "eric/help"
+   
 end
